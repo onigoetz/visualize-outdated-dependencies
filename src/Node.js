3722d0d87d3fcb1864eaf16aafbf12754ff1c478
Stéphane Goetz <onigoetz@onigoetz.ch>
2021-10-18 22:02:31 +0200

    Add size, including children in popup

--- src/Node.js
+++ src/Node.js
@@ -45,7 +45,7 @@ module.exports = class Node {
   isPeerDependency(newDependencyName, newDependencyResolvedVersion) {
     if (
       this.children.some(
-        node =>
+        (node) =>
           node.name === newDependencyName &&
           node.version === newDependencyResolvedVersion
       )
@@ -67,29 +67,48 @@ module.exports = class Node {
   }
 
   getSize() {
-    return this.size /*+ this.children
-      .map(node => {
-        if (node.isCircularDependency(node.name, node.version)) {
-          if (this.verbose) {
-            const pv = `${node.name}@${node.version}`;
-            const parents = node.getParents().join(" => ");
-            console.log(
-              `Circular dependency on ${pv}, cutting here: ${parents}`
-            );
+    return (
+      this.size +
+      this.children
+        .map((node) => {
+          const pv = `${node.name}@${node.version}`;
+          if (this.isCircularDependency(node.name, node.version)) {
+            if (this.verbose) {
+              const parents = node.getParents().join(" => ");
+              console.log(
+                `Circular dependency on ${pv}, cutting here: ${parents}. Skipping for size calculation.`
+              );
+            }
+
+            return 0;
           }
 
-          return 0;
-        }
+          if (this.parent) {
+            const isPeerDependency = this.parent.isPeerDependency(
+              node.name,
+              node.version
+            );
 
-        return node.getSize();
+            if (isPeerDependency) {
+              if (this.verbose) {
+                console.log(
+                  `Dependency ${pv} is already a dependency on one of its parent. Skipping for size calculation.`
+                );
+              }
 
-      })
-      .reduce((previous, current) => previous + current, 0);*/
+              return 0;
+            }
+          }
+
+          return node.getSize();
+        })
+        .reduce((previous, current) => previous + current, 0)
+    );
   }
 
   toArray() {
     const children = this.children
-      .filter(node => {
+      .filter((node) => {
         if (!this.parent) {
           return true;
         }
@@ -107,7 +126,7 @@ module.exports = class Node {
 
         return !isPeerDependency;
       })
-      .map(node => node.toArray());
+      .map((node) => node.toArray());
 
     const size = this.getSize();
 
@@ -119,12 +138,12 @@ module.exports = class Node {
       sizeF: prettyBytes(size),
       children,
       value: children.length || 1,
-      color: GREEN
+      color: GREEN,
     };
 
     // If one child is red, we're orange
     // We don't inherit the color orange more than one level since there is nothing actionable on the package
-    if (children.some(value => value.color === RED)) {
+    if (children.some((value) => value.color === RED)) {
       node.color = ORANGE;
     }
 
